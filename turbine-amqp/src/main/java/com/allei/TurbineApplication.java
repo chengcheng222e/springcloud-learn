5421eeb29ace8c14c8b38d20c31da8c315ad7d5b
chenyuan <chengcheng222e@sina.com>
2019-09-11 20:49:33 +0800

    监控

--- turbine-amqp/src/main/java/com/allei/TurbineApplication.java
+++ turbine-amqp/src/main/java/com/allei/TurbineApplication.java
@@ -3,12 +3,12 @@ package com.allei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableTurbineStream
+@EnableTurbine
 @EnableDiscoveryClient
 public class TurbineApplication {
 
