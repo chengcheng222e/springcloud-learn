7e046523fda554a65a33a07f737823132bd32171
chenyuan <chengcheng222e@sina.com>
2020-01-10 20:38:33 +0800

    配置文件

--- zipkin-server/src/main/java/com/allei/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/allei/ZipkinApplication.java
@@ -3,15 +3,16 @@ package com.allei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @EnableZipkinServer
 @EnableEurekaClient
 @SpringBootApplication
 public class ZipkinApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
 
 }
